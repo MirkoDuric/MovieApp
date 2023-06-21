9949664fe9b3cc29f57bf1a33ee603b7f021fd9f
MirkoDuric <mirkodjuricczv@gmail.com>
2023-06-21 18:28:53 +0200

    Second Carousel with comedy movies added. Need to design the rest of the carousels for other movies. Also finish the search.

--- src/components/MainArea/MainArea.js
+++ src/components/MainArea/MainArea.js
@@ -6,6 +6,7 @@ import "./MainArea.css";
 
 const MainArea = () => {
   const [popularMovieData, setPopularMovieData] = useState([]);
+  const [comedieMovies, setComedieMovies] = useState([]);
   const [isLoading, setIsLoading] = useState();
   const [error, setError] = useState(false);
   useEffect(() => {
@@ -16,6 +17,20 @@ const MainArea = () => {
       )
       .then((response) => {
         setPopularMovieData(response.data.results);
+        console.log(response.data.results);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+        setError(true);
+      });
+    axios
+      .get(
+        `${process.env.REACT_APP_API_URL}/3/discover/movie?api_key=${process.env.REACT_APP_MY_API_KEY}&with_genres=35`
+      )
+      .then((response) => {
+        setComedieMovies(response.data.results);
         console.log(response.data.results);
         setIsLoading(false);
       })
@@ -40,34 +55,64 @@ const MainArea = () => {
         error ? (
           <h3 className="error">Server error! Please try again.</h3>
         ) : (
-          <Carousel className="popular-movies-carousel">
-            {popularMovieData.map((movie, index) => {
-              if (index % 4 === 0) {
-                // Kreira novi red za svaki četvrti film
-                const moviesForRow = popularMovieData.slice(index, index + 4);
+          <div className="carousel-div">
+            <Carousel className="carousel">
+              {popularMovieData.map((movie, index) => {
+                if (index % 4 === 0) {
+                  // Kreira novi red za svaki četvrti film
+                  const moviesForRow = popularMovieData.slice(index, index + 4);
 
-                return (
-                  <Carousel.Item>
-                    <Row className="popular-movies-row">
-                      {moviesForRow.map((movie) => (
-                        <Col key={movie.id} sm={3}>
-                          <MovieCard
-                            img={`${process.env.REACT_APP_IMG_URL}${movie.backdrop_path}`}
-                            movieTitle={movie.original_title}
-                            rating={movie.vote_average}
-                            description={movie.overview}
-                            ratingClass={ratingFilter(movie.vote_average)}
-                          />
-                        </Col>
-                      ))}
-                    </Row>
-                  </Carousel.Item>
-                );
-              } else {
-                return null; // Povratak null za ostale filmove koji nisu na početku reda
-              }
-            })}
-          </Carousel>
+                  return (
+                    <Carousel.Item className="carousel-item">
+                      <Row className="carousel-row">
+                        {moviesForRow.map((movie) => (
+                          <Col key={movie.id} sm={3}>
+                            <MovieCard
+                              img={`${process.env.REACT_APP_IMG_URL}${movie.backdrop_path}`}
+                              movieTitle={movie.original_title}
+                              rating={movie.vote_average}
+                              description={movie.overview}
+                              ratingClass={ratingFilter(movie.vote_average)}
+                            />
+                          </Col>
+                        ))}
+                      </Row>
+                    </Carousel.Item>
+                  );
+                } else {
+                  return null; // Povratak null za ostale filmove koji nisu na početku reda
+                }
+              })}
+            </Carousel>
+            <Carousel className="carousel">
+              {comedieMovies.map((movie, index) => {
+                if (index % 4 === 0) {
+                  // Kreira novi red za svaki četvrti film
+                  const moviesForRow = comedieMovies.slice(index, index + 4);
+
+                  return (
+                    <Carousel.Item className="carousel-item">
+                      <Row className="carousel-row">
+                        {moviesForRow.map((movie) => (
+                          <Col key={movie.id} sm={3}>
+                            <MovieCard
+                              img={`${process.env.REACT_APP_IMG_URL}${movie.backdrop_path}`}
+                              movieTitle={movie.original_title}
+                              rating={movie.vote_average}
+                              description={movie.overview}
+                              ratingClass={ratingFilter(movie.vote_average)}
+                            />
+                          </Col>
+                        ))}
+                      </Row>
+                    </Carousel.Item>
+                  );
+                } else {
+                  return null; // Povratak null za ostale filmove koji nisu na početku reda
+                }
+              })}
+            </Carousel>
+          </div>
         )
       ) : (
         <h3 className="loading">Loading...</h3>
